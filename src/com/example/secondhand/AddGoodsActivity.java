f32846245678a8d838e91c4efa2745754e76dae2
jia-long <995690029@qq.com>
2015-01-13 17:52:01 +0800

    add file

---
+++ src/com/example/secondhand/AddGoodsActivity.java
@@ -0,0 +1,14 @@
+package com.example.secondhand;
+
+import com.example.secondhand.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddGoodsActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_secondhand_addgoods);
+	}
+}
